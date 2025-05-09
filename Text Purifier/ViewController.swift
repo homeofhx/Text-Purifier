e42104e600f3194fe38c1ae5cc92f926eeb15950
homeofhx <178951000+homeofhx@users.noreply.github.com>
2025-05-09 12:31:59 -0600

    Add custom rule box and apply/reset button

--- Text Purifier/ViewController.swift
+++ Text Purifier/ViewController.swift
@@ -1,11 +1,15 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
     @IBOutlet weak var inputBox: NSTextView!
     @IBOutlet weak var displayBox: NSScrollView!
-    @IBOutlet weak var button: NSButton!
+    @IBOutlet weak var rulesBox: NSTextField!
+    @IBOutlet weak var purifyButton: NSButton!
+    @IBOutlet weak var applyRuleButton: NSButton!
+    @IBOutlet weak var resetRuleButton: NSButton!
     
-    private let validCharRegex = try! NSRegularExpression(pattern: "[ -~\\t\\n]", options: [])
+    private var validCharRegex : NSRegularExpression!
     
     @IBAction func purify(_ sender: Any) {
         let inputBoxRawText:String = inputBox.string
@@ -23,10 +27,31 @@ class ViewController: NSViewController {
         displayBox.documentView!.insertText("------ PURIFIED TEXT ------\n\n")
         displayBox.documentView!.insertText(inputBoxPurifiedText + "\n\n")
         displayBox.documentView!.insertText("------ END OF PURIFIED TEXT ------\n\n\n\n\n")
+        print("using rule: " + validCharRegex.description)
+    }
+    
+    @IBAction func applyRule(_ sender: Any) {
+        print("applied btn clicked")
+        let rulesBoxRawString:String = rulesBox.stringValue
+        print("entered rule: " + rulesBoxRawString)
+        validCharRegex = try! NSRegularExpression(pattern: rulesBoxRawString, options: [])
+        print("current rule: " + validCharRegex.description)
+    }
+    
+    @IBAction func resetRule(_ sender: Any) {
+        print("reseted btn clicked")
+        validCharRegex = try! NSRegularExpression(pattern: "[ -~\\t\\n]", options: [])
+        print("current rule: " + validCharRegex.description)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        validCharRegex = try! NSRegularExpression(pattern: "[ -~\\t\\n]", options: [])
+        
+        print("")
+        print("")
+        print("INIT COMPLETE")
+        print("rule: " + validCharRegex.description)
     }
     
     override var representedObject: Any? {
