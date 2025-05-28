a5e0eb2285192d597ef4f75b6b40d634d6676e0e
homeofhx <178951000+homeofhx@users.noreply.github.com>
2025-05-28 00:59:35 -0600

    Fix the bug where the app crashes when pressing the Apply button with no custom rules in the box; add tooltips

--- Text Purifier/ViewController.swift
+++ Text Purifier/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: NSViewController {
     
     private var validCharRegex : NSRegularExpression!
     private let defaultRule = "[ -~\\t\\n]"
+    private let noRuleAlert = NSAlert()
     
     @IBAction func purify(_ sender: Any) {
         let inputFakeTextView = inputBox.documentView as! NSTextView     // A change made in version 2. Similar approach as inputBoxFakeNSString in this function.
@@ -33,15 +34,30 @@ class ViewController: NSViewController {
     
     @IBAction func applyRule(_ sender: Any) {
         let rulesBoxRawString:String = rulesBox.stringValue
-        validCharRegex = try! NSRegularExpression(pattern: rulesBoxRawString, options: [])
+        
+        if rulesBoxRawString.count == 0 {
+            noRuleAlert.runModal()
+        } else {
+            validCharRegex = try! NSRegularExpression(pattern: rulesBoxRawString, options: [])
+            outputBox.documentView!.insertText("CUSTOM PURIFY RULE APPLIED!\n\n")
+        }
     }
     
     @IBAction func resetRule(_ sender: Any) {
         validCharRegex = try! NSRegularExpression(pattern: defaultRule, options: [])
+        outputBox.documentView!.insertText("PURIFY RULE RESET TO DEFAULT! (filters out non-ASCII characters)\n\n")
+    }
+    
+    private func setupNoRuleAlert() {
+        noRuleAlert.messageText = "No Purify Rule!"
+        noRuleAlert.informativeText = "No purify rule in the Custom Rule box. Please add purify rule using regular expression (Regex) in the Custom Rule box before clicking Apply."
+        noRuleAlert.addButton(withTitle: "OK")
+        noRuleAlert.alertStyle = .warning
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNoRuleAlert()
         validCharRegex = try! NSRegularExpression(pattern: defaultRule, options: [])
     }
     
